1e57882dc89e6042a99d88ec9fdea5189ca58550
Carson Bills <CarsonMBills@Gmail.com>
2014-03-03 12:46:37 -0500

    Adds Retwets functionality

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -3,4 +3,27 @@ module TwetsHelper
 		str.gsub(/^@(\w+)$/, '<a href="/\1">@\1</a>').html_safe
 		# The link should be <a href="/username">username</a>
 	end
+
+	def can_retwet(twet)
+		if twet.user_id == current_user.id
+			return false
+		elsif has_retweted(twet)
+			return false
+		else
+			return true
+		end
+	end
+
+
+	def has_retweted(twet)
+		if retwet(twet).present?
+			return true
+		else
+			return false
+		end
+	end
+
+	def retwet(twet)
+		current_user.retwets.where(:twet_id => twet.id).first
+	end
 end
